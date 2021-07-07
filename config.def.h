f9a75a8463ca8f928c0902d88c664e3c98ab8016
AtifChy <iftakhar.awal@yahoo.com>
2021-07-07 15:58:29 +0600

    emoji support & added example xresources file

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-/*	"JetBrainsMono Nerd Font:size=10:antialias=true:autohint=true", */
+	"Noto Color Emoji:pixelsize=13:antialias=true:autohint=true",
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
 
